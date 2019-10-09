30d983f1d4369e3f0a0c181518115e98dca16e50
thenab0303 <thenab0303@gmail.com>
2019-10-09 03:33:54 -0400

    Initial Push

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const backgroundImage = document.querySelector(".backgroundImage");
+const backgroundList = ["background0.jpg", "background1.jpg", "background2.png", "background3.jpg", "background4.png", "background5.png", "background6.jpg"];
+const pageWidth = document.querySelector(".width");
+const pageHeight = document.querySelector(".height");
+
+pageWidth.innerHTML = window.innerWidth;
+pageHeight.innerHTML = window.innerHeight;
+
+const setWidthHeight = (event) => {
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+
+  pageWidth.innerHTML = windowWidth;
+  pageHeight.innerHTML = windowHeight;
+};
+
+window.addEventListener("resize", setWidthHeight);
+
+// backgroundImage.style.backgroundImage = `url("/img/${backgroundList[i]}")`;
+let backgroundIteration = 0;
+const backgroundSlide = () => {
+  backgroundImage.style.backgroundImage = `url("/img/${backgroundList[backgroundIteration]}")`;
+  backgroundIteration++;
+  
+  if (backgroundIteration == backgroundList.length) {
+    backgroundIteration = 0;
+  }; 
+};
+
+setInterval(backgroundSlide, 5000);
